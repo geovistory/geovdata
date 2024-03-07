cccd7930173c024e9a4f58992a43021d51c878ff
Gaétan Muck <gaetan.muck@gmail.com>
2024-03-07 09:12:34 +0100

    update data connection

--- geovdata/sparql/connect.py
+++ geovdata/sparql/connect.py
@@ -6,18 +6,38 @@ def connect_external(url: str, verbose: bool = True) -> None:
     globals.url = url
     if verbose:
         print(f">> External SPARQL URL set to <{url}>")
-    
-    # Temp
-    print('SPARQL URL:', url)
 
 
 def connect_geovistory(pk_project: int = -1) -> None:
     """Connect to Geovistory correct SPARQL endpoint."""
 
-    if  pk_project == -1:
-        path = "https://sparql.geovistory.org/api_v1_community_data"
-    else:
-        path = f"https://sparql.geovistory.org/api_v1_project_{pk_project}"
+    if  pk_project == -1: globals.url = "https://sparql.geovistory.org/api_v1_community_data"
+    else: globals.url = f"https://sparql.geovistory.org/api_v1_project_{pk_project}"
 
-    globals.url = path
     print(f">> SPARQL endpoint of Geovistory project {'COMMUNITY' if pk_project == -1 else pk_project} set.")
+
+
+def connect_well_known(name: str) -> None:
+    """Connect to a well known SPARQL endpoint."""
+
+    if name.lower().strip() == "geovistory":
+        globals.url = "https://sparql.geovistory.org/api_v1_community_data"
+        print('>> SPARQL endpoint of Geovistory Community connected.')
+        return
+
+    if name.lower().strip() == "dbpedia":
+        globals.url = "https://dbpedia.org/sparql/"
+        print('>> SPARQL endpoint of DBpedia connected.')
+        return
+
+    if name.lower().strip() == "wikidata":
+        globals.url = "https://query.wikidata.org/sparql"
+        print('>> SPARQL endpoint of Wikidata connected.')
+        return
+    
+    if name.lower().strip() == "bnf":
+        globals.url = "https://data.bnf.fr/sparql"
+        print('>> SPARQL endpoint of BNF connected.')
+        return
+    
+    raise Exception(f"SPARQL endpoint '{name}' not known")
